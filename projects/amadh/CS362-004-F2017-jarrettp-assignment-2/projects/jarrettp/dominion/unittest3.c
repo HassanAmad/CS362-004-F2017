71d96a0f144a7bbe3c876e6066450965e38606ba
Hassan Amad <hassanamad2007@gmail.com>
2017-11-19 00:41:40 -0500

    Add files via upload

---
+++ projects/amadh/CS362-004-F2017-jarrettp-assignment-2/projects/jarrettp/dominion/unittest3.c
@@ -0,0 +1,55 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+// set NOISY_TEST to 0 to remove printfs from output
+#define NOISY_TEST 1
+
+int main()
+{
+	
+
+	struct gameState G;
+        int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
+           sea_hag, tribute, smithy};
+
+        initializeGame(2, k, 10, &G);
+
+
+	printf("This will test the whoseTurn() and endTurn() functions in dominion.c.\n");
+
+	int who = whoseTurn(&G);	
+	printf("Whose turn? %d\n", who);
+	endTurn(&G);
+	printf("Ending turn..\n");
+
+	who = whoseTurn(&G);
+	printf("Whose turn? %d\n", who);	
+	endTurn(&G);
+	printf("Ending turn..\n");
+
+	who = whoseTurn(&G);
+	printf("Whose turn? %d\n", who);	
+	endTurn(&G);
+	printf("Ending turn..\n");
+
+	who = whoseTurn(&G);
+	printf("Whose turn? %d\n", who);	
+	endTurn(&G);
+	printf("Ending turn..\n");
+
+	who = whoseTurn(&G);
+	printf("Whose turn? %d\n", who);	
+	endTurn(&G);
+	printf("Ending turn..\n");
+
+	who = whoseTurn(&G);
+	printf("Whose turn? %d\n", who);	
+	endTurn(&G);
+	printf("Ending turn..\n\n");
+
+	return 0;
+}
